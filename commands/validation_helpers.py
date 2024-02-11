a5820b0ef87b0591811693db0e4beeaa2c187faa
ina-ene <ina.ene.pancake@gmail.com>
2024-02-11 23:26:07 +0200

    fixed validation_helpers
    - so that they return the input value

--- commands/validation_helpers.py
+++ commands/validation_helpers.py
@@ -43,6 +43,7 @@ def try_parse_int(s):
 def ensure_valid_weight(value):
     if value <= 0.5:
         raise ValueError("Weight should be above 0.5kg")
+    return value
 
 
 def ensure_valid_phone(value):
@@ -51,20 +52,24 @@ def ensure_valid_phone(value):
         raise ValueError("Phone number should be 10 digits long.")
     if not value.isdigit():
         raise ValueError("Phone number should contain only digits ")
+    return value
 
 
 def ensure_valid_email(value):
 
     if not ("@gmail.com" in value or "@yahoo.com" in value):
         raise ValueError("Email should contain '@gmail.com' or '@yahoo.com'")
+    return value
 
 
 def ensure_valid_first_name(value):
     if not 2 <= len(value) <= 10:
         raise ValueError("First name should be between 2 and 10 characters long.")
+    return value
 
 
 def ensure_valid_last_name(value):
 
     if not 2 <= len(value) <= 10:
         raise ValueError("Last name should be between 2 and 10 characters long.")
+    return value

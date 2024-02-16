0ca71388e781a46c434127915bed99b709982e7c
AlexandurDaskalov <AlexandurDaskalov11@gmail.com>
2024-02-16 10:27:43 +0200

    updated Trucks_creation

--- models/vehicles/trucks_creation.py
+++ models/vehicles/trucks_creation.py
@@ -2,22 +2,20 @@ from models.vehicles.actros import Actros
 from models.vehicles.man import Man
 from models.vehicles.scania import Scania
 
-# can't we have just one list Garage, instead of nested lists for easier access
 
-Scania_trucks = []
-Man_truck = []
-Actros_trucks = []
+
+
+Garage = []
 
 for x in range(10):
     truck = Scania()
-    Scania_trucks.append(truck)
+    Garage.append(truck)
 
 for x in range(15):
     truck = Man()
-    Man_truck.append(truck)
+    Garage.append(truck)
 
 for x in range(15):
     truck = Actros()
-    Actros_trucks.append(truck)
+    Garage.append(truck)
 
-Garage = [Scania_trucks, Man_truck, Actros_trucks]

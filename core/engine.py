cc6e91e6ed68fdc0699d9953bd5f1f8df75d0840
Totev42 <150365720+Totev42@users.noreply.github.com>
2024-02-16 10:54:47 +0200

    Saving the system's state implemented

--- core/engine.py
+++ core/engine.py
@@ -4,12 +4,15 @@ from core.command_factory import CommandFactory
 class Engine:
     def __init__(self, factory: CommandFactory):
         self._command_factory = factory
+        self.app_data = self._command_factory._app_data # Link to the App_data of the system
 
     def start(self):
         output: list[str] = []
+        self.app_data.load_system_data() # Load systam data on start.
         while True:
             input_line = input()
             if input_line.lower() == "exit":
+                self.app_data.save_system_data() # Save system data on exit.
                 break
 
             command = self._command_factory.create(input_line)

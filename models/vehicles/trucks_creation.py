3faf532070e07f4433db6e3ec0de1c9f59242b0b
ina-ene <ina.ene.pancake@gmail.com>
2024-02-16 22:25:04 +0200

    Update trucks_creation.py

--- models/vehicles/trucks_creation.py
+++ models/vehicles/trucks_creation.py
@@ -3,8 +3,6 @@ from models.vehicles.man import Man
 from models.vehicles.scania import Scania
 
 
-
-
 Garage = []
 
 for x in range(10):
@@ -19,3 +17,5 @@ for x in range(15):
     truck = Actros()
     Garage.append(truck)
 
+# maybe we can use a list comprehension?
+# Garage = [Scania() for _ in range(10)] + [Man() for _ in range(15)] + [Actros() for _ in range(15)]
